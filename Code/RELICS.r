caaf0cb14698c3cdc2ebcb6681787b87b830b3c7
patfiaux <pfiaux@eng.ucsd.edu>
2019-06-21 12:44:49 -0700

    Updating RELICS

--- Code/RELICS.r
+++ Code/RELICS.r
@@ -397,7 +397,7 @@ calculate_scores <- function(analysis.specs, label.file = NULL){
   data.info <- read.csv(analysis.specs$sgRNAInfoFileLoc,as.is = TRUE, stringsAsFactors = FALSE)
 
   #convert NA in chromosome column to string 'NA'
-  if(length(which(is.na(data.info$chrom)))) > 0){
+  if(length(which(is.na(data.info$chrom))) > 0){
     data.info$chrom[which(is.na(data.info$chrom))] <- 'NA'
   }
   #if a label file is supplied, add corresponding note to the specification file
